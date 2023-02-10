cedc7f3b27de31042e05bb7a40dca9a236039281
cha <chyb627@naver.com>
2023-02-10 23:37:18 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+
+import Add from './pages/Add';
+import Detail from './pages/Detail';
+import Edit from './pages/Edit';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Signin from './pages/Signin';
 
 function App() {
   return (
-    <div>
-      young bin my-diary
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
